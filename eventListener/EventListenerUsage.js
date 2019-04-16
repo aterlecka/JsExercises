9d05d97aa1635746a761df16eb09073a65d24238
aterlecka <aleksandra.terlecka1996@gmail.com>
2019-04-16 20:43:36 +0200

    add up, down functionality to existing list

--- eventListener/EventListenerUsage.js
+++ eventListener/EventListenerUsage.js
@@ -1,4 +1,4 @@
-const aSmallList =  document.getElementById("aSmallList");
+const aSmallList = document.getElementById("aSmallList");
 const listDiv = document.querySelector(".list");
 const input = document.querySelector("input.description2");
 const p = document.querySelector("p.description1");
@@ -6,60 +6,88 @@ const button = document.querySelector("button.description3");
 const allDiv = document.getElementById("all");
 const addInput = document.querySelector("input.addInput");
 const addItemButton = document.querySelector("button.addItemButton");
-const removeItemButton = document.querySelector("button.removeItemButton");
+const listUl = listDiv.querySelector("ul");
+const lis = listUl.children;
 
+function anotherWay(li) {
+    let up = document.createElement("button");
+    up.className = "up";
+    up.textContent = "Up";
+    li.appendChild(up);
 
-listDiv.addEventListener("mouseover", (event) => {
-    if (event.target.tagName ==="LI") {
-        event.target.textContent = event.target.textContent.toUpperCase();
+    let down = document.createElement("button");
+    down.className = "down";
+    down.textContent = "Down";
+    li.appendChild(down);
+
+    let remove = document.createElement("button");
+    remove.className = "remove";
+    remove.textContent = "Remove";
+    li.appendChild(remove);
+
+}
+
+for (let i = 0; i < lis.length; i += 1) {
+    anotherWay(lis[i]);
+}
+
+listUl.addEventListener("click", (event) => {
+    if (event.target.tagName === "BUTTON") {
+        if (event.target.className === "remove") {
+            let li = event.target.parentNode;
+            let ul = li.parentNode;
+            ul.removeChild(li);
+        }
+    }
+    if (event.target.className === "up") {
+        let li = event.target.parentNode;
+        let prevLi = li.previousElementSibling;
+        let ul = li.parentNode;
+        if (prevLi) {
+            ul.insertBefore(li, prevLi);
+        }
+    }
+    if (event.target.className === "down") {
+        let li = event.target.parentNode;
+        let nextLi = li.nextElementSibling;
+        let ul = li.parentNode;
+        if (nextLi) {
+            ul.insertBefore(nextLi, li);
+        }
     }
 });
-listDiv.addEventListener("mouseout", (event) => {
-    if(event.target.tagName ==="LI") {
-        event.target.textContent = event.target.textContent.toLowerCase();
-    }
-});
 
-
-
-
-aSmallList.addEventListener("click", ()=>  {
-    if ( listDiv.style.display === "none"){
+aSmallList.addEventListener("click", () => {
+    if (listDiv.style.display === "none") {
         aSmallList.textContent = "Hide List";
         listDiv.style.display = "block";
-    } else{
+    } else {
         aSmallList.textContent = "Show List";
         listDiv.style.display = "none";
     }
-} );
+});
 
-
-
-button.addEventListener("click", () =>{
+button.addEventListener("click", () => {
     p.innerHTML = input.value + " Suprise! ";
-    input.value="";
+    input.value = "";
 });
 
 allDiv.addEventListener("mouseover", () => {
-    allDiv.style.color="red";
+    allDiv.style.color = "red";
 });
 allDiv.addEventListener("mouseout", () => {
-    allDiv.style.color="black";
+    allDiv.style.color = "black";
 });
-
 
 addItemButton.addEventListener("click", () => {
     let ul = document.getElementsByTagName("ul")[0];
     let li = document.createElement("li");
-    li.textContent=addInput.value;
+    li.textContent = addInput.value;
+    anotherWay(li);
     ul.appendChild(li);
-    addInput.value="";
+    addInput.value = "";
 });
 
 
-removeItemButton.addEventListener("click", () => {
-    let ul = document.getElementsByTagName("ul")[0];
-   let li = document.querySelector("li:last-child");
-   ul.removeChild(li);
 
-});
+
